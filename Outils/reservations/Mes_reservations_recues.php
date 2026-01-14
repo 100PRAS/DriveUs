7a9e42f0feb91b5298364ecb3f609b00e27735d1
100PRAS <samprasatonkoum@gmail.com>
2026-01-14 20:52:46 +0100

    Fix include paths: use __DIR__ for absolute paths

--- Outils/reservations/Mes_reservations_recues.php
+++ Outils/reservations/Mes_reservations_recues.php
@@ -2,8 +2,8 @@
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
-require_once '../config/langue.php';
-require_once '../config/config.php';
+require_once __DIR__ . '/../config/langue.php';
+require_once __DIR__ . '/../config/config.php';
 
 // Vérifier si l'utilisateur est connecté
 if (!isset($_SESSION['UserID'])) {
